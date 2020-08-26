274bb6cad9efe73cddd38fd2fe98adaa60a79ce2
Safiyah Lakhany <salakhany@gmail.com>
2020-08-25 21:19:15 -0700

    Updates

---
+++ precache-manifest.a1c23ec06ef2cafe7b133dcdccabe27e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611f0283e7de81b55dbdfdedcf29b43c",
+    "url": "/Pop/index.html"
+  },
+  {
+    "revision": "29222d558e21fabe849b",
+    "url": "/Pop/static/css/main.d4c9281c.chunk.css"
+  },
+  {
+    "revision": "df20d0e2902b4b90a26d",
+    "url": "/Pop/static/js/2.89c4eca5.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/Pop/static/js/2.89c4eca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29222d558e21fabe849b",
+    "url": "/Pop/static/js/main.64eb7dd4.chunk.js"
+  },
+  {
+    "revision": "ea4140ff307af2144d3f",
+    "url": "/Pop/static/js/runtime-main.660cc675.js"
+  },
+  {
+    "revision": "e40db72324470dec31447018bc52f438",
+    "url": "/Pop/static/media/BubblesBanner.e40db723.png"
+  },
+  {
+    "revision": "2b16b3707c6e3845cba339b83c201042",
+    "url": "/Pop/static/media/Option1ProfPic.2b16b370.png"
+  },
+  {
+    "revision": "39678302e6ffb68b2322a7e2e62a9fcc",
+    "url": "/Pop/static/media/Option2ProfPic.39678302.png"
+  },
+  {
+    "revision": "81edc7ccac4fd5c622b32d187cc9f327",
+    "url": "/Pop/static/media/Option3ProfPic.81edc7cc.png"
+  },
+  {
+    "revision": "1cfaac493b8888f613a4684547f7ab2d",
+    "url": "/Pop/static/media/PopLogo.1cfaac49.png"
+  },
+  {
+    "revision": "b33ca93ab0b33be57564f0cba5abb19c",
+    "url": "/Pop/static/media/profPic.b33ca93a.png"
+  }
+]);
